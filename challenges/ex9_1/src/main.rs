ed0e95c5b1675d7dae8535b282fd3a357efbcd52
Enrico Meloni <enrico.meloni@outlook.it>
2024-07-28 13:04:58 +0200

    Solved challenge 9.1

---
+++ challenges/ex9_1/src/main.rs
@@ -0,0 +1,49 @@
+use itertools::Itertools;
+use utils::{get_input_path, read_lines};
+
+fn get_differences(seq: &Vec<i32>) -> Vec<i32> {
+    let mut diff_seq = Vec::with_capacity(seq.len() - 1);
+
+    for idx in (1..seq.len()).rev() {
+        diff_seq.push(seq[idx] - seq[idx - 1]);
+    }
+
+    diff_seq.reverse();
+    diff_seq
+}
+
+fn get_next_value(seq: &Vec<i32>) -> i32 {
+    if seq.iter().all(|&v| v == 0) {
+        return 0;
+    } else {
+        let last = seq.last().unwrap();
+        let differences = get_differences(seq);
+        let next_value = get_next_value(&differences);
+        return last + next_value;
+    }
+}
+
+fn main() {
+    let input_path = get_input_path(9, Some("input.txt"));
+
+    if let Ok(lines) = read_lines(&input_path) {
+        let lines = lines.map(|line| line.unwrap()).collect_vec();
+
+        let sequences = lines
+            .iter()
+            .map(|line| {
+                line.split_whitespace()
+                    .map(|number_str| number_str.parse::<i32>().unwrap())
+                    .collect_vec()
+            })
+            .collect_vec();
+
+        let next_values = sequences.iter().map(get_next_value).collect_vec();
+
+        let sum: i32 = next_values.iter().sum();
+
+        println!("{:?}", next_values);
+
+        println!("The sum is {sum}");
+    }
+}
